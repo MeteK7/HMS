258dd52ec1f3e14720b842b2c3b84801b72d79ae
mete <metekaba96@gmail.com>
2019-03-23 12:45:33 +0300

    Coding for Main Window and Doctor Login Window

--- hmsProject/DocLogin.xaml.cs
+++ hmsProject/DocLogin.xaml.cs
@@ -23,5 +23,64 @@ namespace hmsProject
         {
             InitializeComponent();
         }
+
+
+        private void txtDocName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtDocName.Text == "NAME")
+            {
+                txtDocName.Text = "";
+            }
+
+            if (String.IsNullOrEmpty(txtDocSName.Text))
+            {
+                txtDocSName.Text = "SURNAME";
+            }
+        }
+
+        private void txtDocSName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtDocSName.Text == "SURNAME")
+            {
+                txtDocSName.Text = "";
+            }
+
+            if (String.IsNullOrEmpty(txtDocName.Text))
+            {
+                txtDocName.Text = "NAME";
+            }
+        }
+
+        private void pswDoc_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtDocName.Text))
+            {
+                txtDocName.Text = "NAME";
+            }
+
+            if (String.IsNullOrEmpty(txtDocSName.Text))
+            {
+                txtDocSName.Text = "SURNAME";
+            }
+        }
+
+        private void btnDocLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDocName.Text == "" || txtDocName.Text == "NAME" || txtDocSName.Text == "" || txtDocSName.Text == "SURNAME" || pswDoc.Password == "")
+            {
+                lblWarning.Content = "Please fill all fields.";
+            }
+            else
+                CheckAccount();
+        }
+
+        private void CheckAccount()
+        {
+        }
+
+        private void btnPassRequ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Contact your administrator to reset your password!");
+        }
     }
 }
