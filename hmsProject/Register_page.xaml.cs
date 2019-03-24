8318615322d96ba2d253e165898acc3eae400f1c
teynurs <teynur.yildiz@gmail.com>
2019-03-24 16:27:11 +0300

    added github => register_page

---
+++ hmsProject/Register_page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hmsProject
+{
+    /// <summary>
+    /// Register_page.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Register_page : Window
+    {
+        public Register_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
